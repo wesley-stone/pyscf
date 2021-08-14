b5558c71e12e83b78c4b177ad6bb131dfbe4e151
wenshi <wenshi@microsoft.com>
2021-08-14 19:18:59 +0800

    fix bugs

--- eri/__init__.py
+++
@@ -1,5 +0,0 @@
-from .eri_cal import generate_h2o_data
-
-def call(cfg, logger):
-    if cfg.task == 'collect_data':
-        generate_h2o_data(cfg, logger)
--- utils.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def create_if_not_exist(path):
-    if os.path.exists(path):
-        return
-    os.makedirs(path)
